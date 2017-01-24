f91af1fa87bea1a527831f25eec21b3c4077687e
nicoShultz <nico.g.shultz@gmail.com>
2017-01-24 13:25:48 +0100

    fixed the post

--- public/js/post.js
+++ public/js/post.js
@@ -11,19 +11,35 @@ function PostData()
         course: questCourse,
         info: questInfo
     };
-
     var marker = [];
 
     for(i = 0; i < markers.length; i++)
     {
-        marker.push(markers[i].metadata);
+        marker.push({
+            name: markers[i].metadata.name,
+            markerInfo: markers[i].metadata.markerInfo,
+            isQr: markers[i].metadata.isQR,
+            isVisible: markers[i].metadata.isVisible,
+            location: {
+                lat: markers[i].metadata.location.lat,
+                lng: markers[i].metadata.location.lng
+            },
+            questions: {
+                answer1: markers[i].metadata.questions.answer1,
+                answer2: markers[i].metadata.questions.answer2,
+                answer3: markers[i].metadata.questions.answer3,
+                answer4: markers[i].metadata.questions.answer4,
+                correctAnswer: markers[i].metadata.questions.correctAnswer,
+                points: markers[i].metadata.questions.points,
+                question: markers[i].metadata.questions.question
+            }
+        });
     }
-
     var data = {
         quest:quest,
         marker:marker
-    }
-    console.log(data);
+    };
+    console.log(JSON.stringify(data));
     $.ajax({
             type: "POST",
             url: url + 'postQuest/',
